007d19d625b173a05a4bee709207bdb15fa27580
Liam Gallagher <lg319@ic.ac.uk>
2020-09-22 18:18:46 +0100

    refactored code this library. fixed a bug regarding the stack length

--- src/forthmachine/forthmachine.c
+++ src/forthmachine/forthmachine.c
@@ -8,42 +8,51 @@
 
 #define INITIAL_STACK_SIZE 128
 
-bool ForthMachine_init(struct forth_machine *fmach)
+struct forth_machine *forth_machine_init()
 {
+    struct forth_machine *fmach = (struct forth_machine *) malloc(
+                                  sizeof(struct forth_machine)
+                                  );
+    if(fmach == NULL)
+    {
+        return NULL;
+    }
     assert(fmach != NULL);
     Trie *words = Trie_create();
     if(words == NULL)
     {
-        return false;
+        return NULL;
     } 
     Trie *variables = Trie_create();
     if(variables == NULL)
     {
-        Trie_destroy(words, free);
-        return false;
+        // Trie_destroy(words, free); need to give a different function pointer 
+        return NULL;
     }
     ForthStack *stack = ForthStack_create(INITIAL_STACK_SIZE);
     if(stack == NULL)
     {
-        Trie_destroy(words, free);
-        Trie_destroy(variables, free);
-        return false;
+        // Trie_destroy(words, free);
+        // Trie_destroy(variables, free);
+        return NULL;
     }
+    ForthStack_adjust_length(&stack, 0);
+
     fmach->words = words;
     // TODO: populate this trie  
     fmach->variables = variables;
     fmach->stack = stack;
     fmach->program_words = NULL;
     fmach->n_program_words = 0;
-    fmach->program_counter = 0
-    return true;   
+    fmach->program_counter = 0;
+    return fmach;   
 }
 
-void ForthMachine_deinit(struct forth_machine *fmach)
+void forth_machine_deinit(struct forth_machine *fmach)
 {
     assert(fmach != NULL);
-    Trie_destroy(fmach->words, free);
-    Trie_destroy(fmach->variables, free);
+    // Trie_destroy(fmach->words, free);
+    // Trie_destroy(fmach->variables, free);
     ForthStack_destroy(fmach->stack);
     if(fmach->program_words == NULL)
     { 
@@ -51,7 +60,7 @@ void ForthMachine_deinit(struct forth_machine *fmach)
     }
 }
 
-bool ForthMachine_load_program(struct forth_machine *fmach, const char *program)
+bool forth_machine_load_program(struct forth_machine *fmach, const char *program)
 {
     assert(fmach != NULL && program != NULL);  
     fmach->n_program_words = num_words(program);
@@ -60,6 +69,6 @@ bool ForthMachine_load_program(struct forth_machine *fmach, const char *program)
     return fmach->program_words != NULL;
 }
 
-enum error_code ForthMachine_run_program(struct forth_machine);
+enum error_code forth_machine_run_program(struct forth_machine);
 
 
