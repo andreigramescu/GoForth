8fc7ed9eab4944d4017082039b3715a880bee2bf
Andrei <andrei@andrei-laptop.home>
2020-09-22 14:39:43 +0300

    Added colors to test lib output

--- test/test.c
+++ test/test.c
@@ -3,5 +3,14 @@
 #include "test.h"
 
 void test_bool(bool toTest, const char *message) {
-  printf("T: %s ----- %s\n", message, toTest ? "PASS" : "FAIL");
+  printf("T: %s ----- ", message);
+  if (toTest) {
+    printf("\033[1;32m"); 
+    printf("PASS\n");
+    printf("\033[0m"); 
+    return;
+  }
+  printf("\033[1;31m");
+  printf("FAIL\n");
+  printf("\033[0m"); 
 }
