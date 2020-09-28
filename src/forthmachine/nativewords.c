c64631d5a7e10368fcef816c59bfba92bb717392
Andrei Gramescu <andrei.gramescu@gmail.com>
2020-09-28 20:48:09 +0100

    Finished native words

--- src/forthmachine/nativewords.c
+++ src/forthmachine/nativewords.c
@@ -5,13 +5,81 @@
 #include "forthstack.h"
 
 static char *get_curr_word(struct forth_machine *fmach) {
-  return fmach->program_words[fmach->n_program_words];
+  return fmach->program_words[fmach->program_counter];
 }
 
 enum error_code word_number(struct forth_machine *fmach) {
   char *endptr;
   int64_t val = strtoimax(get_curr_word(fmach), &endptr, 0);
   ForthStack_append(&(fmach->stack), val);
-  (fmach->program_counter)++;
   return EXECUTE_OK;
  }
+
+enum error_code word_dup(struct forth_machine *fmach) {
+  ForthStack_append(&(fmach->stack), ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1));
+  return EXECUTE_OK;
+}
+
+enum error_code word_drop(struct forth_machine *fmach) {
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  return EXECUTE_OK;
+}
+
+enum error_code word_plus(struct forth_machine *fmach) {
+  int64_t top1 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  int64_t top2 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_append(&(fmach->stack), top1 + top2);
+  return EXECUTE_OK;
+}
+
+enum error_code word_minus(struct forth_machine *fmach) {
+  int64_t top1 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  int64_t top2 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_append(&(fmach->stack), top2 - top1);
+  return EXECUTE_OK;
+}
+
+
+enum error_code word_star(struct forth_machine *fmach) {
+  int64_t top1 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  int64_t top2 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_append(&(fmach->stack), top1 * top2);
+  return EXECUTE_OK;
+}
+
+enum error_code word_slash(struct forth_machine *fmach) {
+  int64_t top1 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  int64_t top2 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_append(&(fmach->stack), top2 / top1);
+  return EXECUTE_OK;
+}
+
+enum error_code word_mod(struct forth_machine *fmach) {
+  int64_t top1 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  int64_t top2 = ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_remove(fmach->stack, ForthStack_length(fmach->stack) - 1);
+  ForthStack_append(&(fmach->stack), top2 % top1);
+  return EXECUTE_OK;
+}
+
+enum error_code word_dot(struct forth_machine *fmach) {
+  printf("%ld\n", ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1));
+  return EXECUTE_OK;
+}
+
+enum error_code word_dot_s(struct forth_machine *fmach) {
+  for (int i = 0; i < ForthStack_length(fmach->stack); i++) {
+    printf("%ld ", ForthStack_get(fmach->stack, i));
+  }
+  puts("\n");
+  return EXECUTE_OK;
+}
--- src/forthmachine/nativewords_tests.c
+++ src/forthmachine/nativewords_tests.c
@@ -16,14 +16,61 @@ int main(void) {
   // Build forth machine
   ForthStack_append(&(fmach->stack), 2);
   ForthStack_append(&(fmach->stack), 8);
-  fmach->program_words = make_words_array("90 34", 2);
+  fmach->program_words = make_words_array("90 2 96 2", 4);
 
   test_bool(ForthStack_length(fmach->stack) == 2, "stack size is initially 2");
   word_number(fmach);
   test_bool(ForthStack_length(fmach->stack) == 3, "stack size has grown by 1 after appending a number");
   test_bool(ForthStack_get(fmach->stack, 2) == 90, "appended 90 at the end of the stack after executing 90");
-  test_bool(fmach->program_counter == 1, "program counter was incremented");
+  word_dot_s(fmach);
 
+  puts("\nRunning duplicate word...");
+  word_dup(fmach);
+  test_bool(ForthStack_length(fmach->stack) == 4, "stack size has grown by 1 after duplicating top item");
+  test_bool(ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1) == 90, "Correctly duplicated 90");
+
+  puts("\nRunning drop word...");
+  word_drop(fmach);
+  test_bool(ForthStack_length(fmach->stack) == 3, "stack length has droped by 1 after dropping top item");
+  test_bool(ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1) == 90, "correctlu dropped 90");
+
+  puts("\nRunning plus command..."); 
+  word_plus(fmach);
+  test_bool(ForthStack_length(fmach->stack) == 2, "stack length has dropped by 1 after adding top items");
+  test_bool(ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1) == 98, "Correctly added the top 2 items from the stack");
+  word_dot(fmach);
+
+  puts("\nRunning minus command..."); 
+  word_minus(fmach);
+  test_bool(ForthStack_length(fmach->stack) == 1, "stack length has dropped by 1 after subtracting top items");
+  test_bool(ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1) == -96, "Correctly subtracted the top 2 items from the stack");
+  word_dot(fmach);
+
+
+  (fmach->program_counter)++;
+  word_number(fmach);
+  puts("\nRunning star command..."); 
+  word_star(fmach);
+  test_bool(ForthStack_length(fmach->stack) == 1, "stack length has dropped by 1 after multiplying top items");
+  test_bool(ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1) == -192, "Correctly multiplied the top 2 items from the stack");
+
+
+
+  (fmach->program_counter)++;
+  word_number(fmach);
+  puts("\nRunning slash command..."); 
+  word_slash(fmach);
+  test_bool(ForthStack_length(fmach->stack) == 1, "stack length has dropped by 1 after multiplying top items");
+  test_bool(ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1) == -2, "Correctly divided the top 2 items from the stack");
+
+
+  (fmach->program_counter)++;
+  word_number(fmach);
+  puts("\nRunning mod command..."); 
+  word_mod(fmach);
+  test_bool(ForthStack_length(fmach->stack) == 1, "stack length has dropped by 1 after modding top items");
+  test_bool(ForthStack_get(fmach->stack, ForthStack_length(fmach->stack) - 1) == 0, "Correctly modded the top 2 items from the stack");
+  word_dot(fmach);
   // ForthMachine_deinit(fmach);
   return 0;
 }
